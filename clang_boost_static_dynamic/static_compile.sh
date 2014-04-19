de43746a0a38ef1e27026fe9ac2f04f0e1fea71d
fvdb <frank@am-knie.de>
2014-04-19 17:04:45 +0200

    add clang example

---
+++ clang_boost_static_dynamic/static_compile.sh
@@ -0,0 +1,9 @@
+
+
+# das geht ist zwar ohne libc++ aber erstmal egal
+
+clang++ -std=c++11 -I /usr/include/boost/ -L /opt/boost/boost_1_55_0/stage/lib -o static_file file.cpp -lboost_system -lboost_filesystem  -static
+
+
+
+clang++ -std=c++11 -I /usr/include/boost/ -L /opt/boost/boost_1_55_0/stage/lib -o static_file2 file2.cpp -lboost_system -lboost_filesystem  -static
